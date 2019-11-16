8a0c912435816a4ecbd824afbec157b3771f8901
Cohen <dcoco91@gmail.com>
2019-11-16 15:05:23 -0500

    Updates

---
+++ precache-manifest.492481ee918febf05c442f1997d00e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f8192af9258b72f09ae7edbf4f78ea",
+    "url": "/SynonymFinder/index.html"
+  },
+  {
+    "revision": "3aeb4ecff1aab693eafe",
+    "url": "/SynonymFinder/static/css/main.53c671ff.chunk.css"
+  },
+  {
+    "revision": "e2acf8bb6ac39569e2e0",
+    "url": "/SynonymFinder/static/js/2.aa5f73ab.chunk.js"
+  },
+  {
+    "revision": "3aeb4ecff1aab693eafe",
+    "url": "/SynonymFinder/static/js/main.12575496.chunk.js"
+  },
+  {
+    "revision": "cc3fd419f54185804331",
+    "url": "/SynonymFinder/static/js/runtime-main.ee9eea2c.js"
+  },
+  {
+    "revision": "f5d951bd681714b034b0118226d0f1bd",
+    "url": "/SynonymFinder/static/media/mwlogo.f5d951bd.jpg"
+  }
+]);
